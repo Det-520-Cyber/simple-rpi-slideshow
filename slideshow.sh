81733a209c73c4ccde4156728635dbcf96d55143
Jakob Nacanaynay <jnac8080@gmail.com>
2025-08-30 14:41:46 -0400

    Change display time

--- slideshow.sh
+++ slideshow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 unclutter &
-feh -FrzD 5 --hide-pointer --slideshow-delay 5 --auto-zoom /home/det520/images
+feh -FrzD 5 --hide-pointer --slideshow-delay 8 --auto-zoom /home/det520/images
